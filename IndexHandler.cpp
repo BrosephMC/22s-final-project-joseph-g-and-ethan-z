c9e64667b2a071ce6ea3e6d888095b065d579b5b
BrosephMC <jojoglick418@gmail.com>
2022-04-11 16:46:15 -0500

    classes

---
+++ IndexHandler.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jojog on 4/11/2022.
+//
+
+#include "IndexHandler.h"
--- main.cpp
+++ main.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
 
+#include include/rapidjson/document.h
+
 int main() {
     std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
-    std::cout << "Hello, World!" << std::endl;
+
+    rapidjson::Document doc;
+    string wholeFile
+    // input the whole .json into wholeFile
+    doc.Parse();
+
+    //DocumentParser.parsedocuments(asdfsadf)
+    //
+
     return 0;
 }
