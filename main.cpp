8f597971a8a03482dc29504336a1ae41d6adf4f5
BrosephMC <jojoglick418@gmail.com>
2022-04-12 14:51:17 -0500

    rapidjson testing

--- main.cpp
+++ main.cpp
@@ -1,21 +1,47 @@
 #include <iostream>
+#include <fstream>
 
-#include include/rapidjson/document.h
+#include "include/rapidjson/document.h"
+#include "include/rapidjson/writer.h"
+#include "include/rapidjson/stringbuffer.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    using namespace std;
+
+    cout << "Hello, World!" << endl;
+
+    int maxCharCount = 10000;
+    char wholeFile[maxCharCount];
+
+    fstream data_file;
+    data_file.open("temporary_data/blogs_0000001.json");
+    data_file.getline(wholeFile, maxCharCount);
+    data_file.close();
+    cout << wholeFile << endl;
 
     rapidjson::Document doc;
-    string wholeFile
-    // input the whole .json into wholeFile
-    doc.Parse();
+    doc.Parse(wholeFile);
+    cout << doc["uuid"].GetString() << endl;
 
-    //DocumentParser.parsedocuments(asdfsadf)
-    //
+    // 1. Parse a JSON string into DOM.
+    //const char* json = "{\"project\":\"rapidjson\",\"stars\":10}";
+    //rapidjson::Document doc;
+    //doc.Parse(json);
+
+    // 2. Modify it by DOM.
+    //rapidjson::Value& s = doc["stars"];
+    //s.SetInt(s.GetInt() + 1);
+
+    // 3. Stringify the DOM
+    //rapidjson::StringBuffer buffer;
+    //rapidjson::Writer<rapidjson::StringBuffer> writer(buffer);
+    //doc.Accept(writer);
+
+    // Output {"project":"rapidjson","stars":11}
+    //std::cout << buffer.GetString() << std::endl;
+    //cout << doc["project"].GetString() << endl;
 
     return 0;
+    //DocumentParser.parsedocuments(asdfsadf);
 }
-
-//this is some comment code
-
-//more test code
