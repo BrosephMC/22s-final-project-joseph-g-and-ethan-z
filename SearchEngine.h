d3bc7fceac97ad13c26e4cf027daef0a370ea8d0
EthanZech <EthanWZech@gmail.com>
2022-04-12 22:39:49 -0500

    - Added search() function definition to SearchEngine.h

--- SearchEngine.h
+++ SearchEngine.h
@@ -1,14 +1,20 @@
 //
-// Created by jojog on 4/11/2022.
+// Owner - Ethan Zech
 //
 
 #ifndef INC_22S_FINAL_PROJECT_JOSEPH_G_AND_ETHAN_Z_SEARCHENGINE_H
 #define INC_22S_FINAL_PROJECT_JOSEPH_G_AND_ETHAN_Z_SEARCHENGINE_H
 
+#include <string>
 
 class SearchEngine {
-
+public:
+    /**
+     * Returns the articles which best fit the entered query.
+     * TODO: Figure out a return type
+     * @param query - Entered search terms.
+     */
+    void search(const std::string& query);
 };
-
 
 #endif //INC_22S_FINAL_PROJECT_JOSEPH_G_AND_ETHAN_Z_SEARCHENGINE_H
