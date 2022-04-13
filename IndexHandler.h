6aec82d39d20c18a94d1926f439a9218c511661a
EthanZech <EthanWZech@gmail.com>
2022-04-12 22:36:45 -0500

    - Added constructor and equality operators to WordData.h - Wrote function declarations in IndexHandler.h

--- IndexHandler.h
+++ IndexHandler.h
@@ -1,13 +1,38 @@
 //
-// Created by jojog on 4/11/2022.
+// Owner - Ethan Zech
 //
 
 #ifndef INC_22S_FINAL_PROJECT_JOSEPH_G_AND_ETHAN_Z_INDEXHANDLER_H
 #define INC_22S_FINAL_PROJECT_JOSEPH_G_AND_ETHAN_Z_INDEXHANDLER_H
 
+#include "DSAVLTree.h"
+#include "WordData.h"
+#include <string>
 
 class IndexHandler {
+private:
+    DSAVLTree<WordData> wordIndex;
 
+public:
+    /**
+     * Adds the given word to the word index.
+     * @param word - Word to be added to the index.
+     */
+    void insertWord(const std::string& word);
+
+    /**
+     * Inserts the word if it isn't already in the index then adds the articleID to that word.
+     * @param word - Word to find or create in the index.
+     * @param articleID - ID of the article the word was found in.
+     */
+    void indexWord(const std::string& word, const std::string& articleID);
+
+    /**
+     * Returns a reference to the WordData that corresponds with the entered word.
+     * @param word - Word to return.
+     * @return WordData that corresponds with the entered word.
+     */
+    WordData& getWord(const std::string& word);
 };
 
 
--- WordData.h
+++ WordData.h
@@ -11,15 +11,18 @@
 struct WordData{
 public:
     //Indexed word
-    string word("");
+    std::string word;
     //Articles that contain the word
-    vector<string> articles;
+    std::vector<std::string> articles;
+
+    //Constructors and Destructors
+    WordData(const std::string& w) { word = w; }
 
     //Overloaded Operators
     //Checks if the two WordDatas contain the same data
     bool operator== (const WordData&) const;
     //Checks if word equals the inputted string
-    bool operator== (const string&) const;
+    bool operator== (const std::string&) const;
 };
 
 #endif //INC_22S_FINAL_PROJ_WORDDATA_H
