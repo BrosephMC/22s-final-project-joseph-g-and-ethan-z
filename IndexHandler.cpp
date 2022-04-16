dc47a7e301c290c75ca6b654cd6ea8cd44030880
EthanZech <EthanWZech@gmail.com>
2022-04-16 16:29:00 -0500

    - Started work on IndexHandler.cpp

--- IndexHandler.cpp
+++ IndexHandler.cpp
@@ -3,3 +3,14 @@
 //
 
 #include "IndexHandler.h"
+
+void IndexHandler::insertWord(const std::string& word) {
+    wordIndex.insert(WordData(word));
+}
+
+void IndexHandler::indexWord(const std::string& word, const std::string& articleID) {
+    if(!wordIndex.contains(word)){
+        insertWord(word);
+    }
+
+}
