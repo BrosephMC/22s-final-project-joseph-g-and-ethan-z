95af31abd4011f18a96a63c9759922e69abf47a8
EthanZech <EthanWZech@gmail.com>
2022-04-18 17:59:27 -0500

    - Put SearchEngine in main

--- main.cpp
+++ main.cpp
@@ -1,27 +1,13 @@
 #include <iostream>
 
-#include "DocumentParser.h"
-#include "IndexHandler.h"
-#include "QueryProcessor.h"
+#include "SearchEngine.h"
 
 int main(int argc, char** argv) {
 
     using namespace std;
 
-    cout << "Hello, World!" << endl;
-
-    cout << argv[1] << endl;
-
-    DocumentParser document;
-    IndexHandler ih;
-    QueryProcessor q;
-
-    //document.ParseDocument("/mnt/c/Users/jojog/Downloads/archive/2018_01_112b52537b67659ad3609a234388c50a/news_0034753.json");
-    //document.ParseText(document.returnString("text").c_str());
-
-    document.ParseDatabase(argv[1], ih);
-
-    q.Search("Gordon", ih);
+    SearchEngine searchEngine(argv[2]);
+    searchEngine.parseDatabase(argv[1]);
 
     return 0;
 }
