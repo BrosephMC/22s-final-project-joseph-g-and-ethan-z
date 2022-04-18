dec5e56fd867c425e8ac3b492613c162c0f33deb
BrosephMC <jojoglick418@gmail.com>
2022-04-18 18:02:27 -0500

    small changes in main for SearchEngine

--- main.cpp
+++ main.cpp
@@ -7,7 +7,8 @@ int main(int argc, char** argv) {
     using namespace std;
 
     SearchEngine searchEngine(argv[2]);
-    searchEngine.parseDatabase(argv[1]);
+    searchEngine.parseDatabase(argv[2]);
+    searchEngine.search(argv[1]);
 
     return 0;
 }
