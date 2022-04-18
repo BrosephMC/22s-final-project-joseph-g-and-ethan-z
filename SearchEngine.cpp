5aa40811f3d0c57ed9dfc28286788f454ed105c0
EthanZech <EthanWZech@gmail.com>
2022-04-18 17:56:49 -0500

    - Implemented SearchEngine.h input functions.

--- SearchEngine.cpp
+++ SearchEngine.cpp
@@ -3,3 +3,15 @@
 //
 
 #include "SearchEngine.h"
+
+SearchEngine::SearchEngine(const string &dataBase) : dataBasePath(dataBase) {
+
+}
+
+void SearchEngine::parseDatabase(char*& path) {
+    document.ParseDatabase(path, ih);
+}
+
+void SearchEngine::search(const string& query) {
+    q.Search(query, ih);
+}
--- SearchEngine.h
+++ SearchEngine.h
@@ -5,10 +5,25 @@
 #ifndef INC_22S_FINAL_PROJECT_JOSEPH_G_AND_ETHAN_Z_SEARCHENGINE_H
 #define INC_22S_FINAL_PROJECT_JOSEPH_G_AND_ETHAN_Z_SEARCHENGINE_H
 
+#include "DocumentParser.h"
+#include "IndexHandler.h"
+#include "QueryProcessor.h"
 #include <string>
 
 class SearchEngine {
+private:
+    string dataBasePath = "";
+
+    DocumentParser document;
+    IndexHandler ih;
+    QueryProcessor q;
+
 public:
+    //Constructors and Destructors
+    SearchEngine(const string& dataBase);
+
+    void parseDatabase(char*& path);
+
     /**
      * Returns the articles which best fit the entered query.
      * TODO: Figure out a return type
